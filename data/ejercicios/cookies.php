e6d71bfe2dd7f585306b8c598555f5b49f424681
Pablo Train <pablotrain25@gmail.com>
2022-10-19 17:34:52 +0100

    ac

--- data/ejercicios/cookies.php
+++ data/ejercicios/cookies.php
@@ -6,9 +6,14 @@ time(); //epoch time, unix -> Tiempo actual en segundos desde 1970
 //Definir una cookie
 
 $miarray = ["hola", 25, [1,3,5]];
+$miarrayjson = ['a'=>"hola", 'b'=>25, 'c'=>[1,3,5]];
 $infoguardar = serialize($miarray);//Devuelve una cadena, en este cado guadamos un array
 
-setcookie("primeracookie", "aceptado", time()+300);
+
+$infojson = json_encode($miarrayjson);
+
+
+setcookie("primeracookie", $infoguardar, time()+300);
 setcookie("segundacookie", $infoguardar, time()+600);
 
 //Caducar una cookie
@@ -16,19 +21,9 @@ setcookie("segundacookie", $infoguardar, time()+600);
 
 echo "<h1>Mi primera cookie</h1>";
 echo "Valor de la primera cookie: " . $_COOKIE["primeracookie"];
-echo "<pre>",
-var_dump(unserialize($_COOKIE["segundacookie"]));//La cadena vuelve a como estaba sin serialize
+echo "<pre>";
+echo "Informacion de la 2º cookie: ";
+var_dump($infojson);
+//var_dump(unserialize($_COOKIE["segundacookie"]));//La cadena vuelve a como estaba sin serialize
 
 echo "<br>Número de cookies creadas: " . count($_COOKIE);
-
-
-
-
-
-
-
-
-
-
-
-
